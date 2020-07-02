befcfbc2f6141b4d1e7adba3c7906a1e0d1a8c48
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-07-02 01:16:58 +0100

    Finally completed this thing

--- login-form/index.js
+++ login-form/index.js
@@ -23,18 +23,79 @@ const throttle = (func, limit) => {
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const dog = document.querySelector('.face');
+const hands = document.querySelectorAll('.hand');
+const tongue = document.querySelector('.tongue');
+const toggle = document.querySelector('.show-pass');
 
+// Rotate head on focus
 email.addEventListener('focus', () => {
   dog.style.setProperty('--rotate-face', '15deg');
-})
+  hands.forEach(hand => {
+    hand.classList.remove('cover');
+    hand.classList.remove('peek');
+  });
+});
 
+// Rotate head when input field loses focus
 email.addEventListener('blur', () => {
   dog.style.setProperty('--rotate-face', '0deg');
-})
+});
 
-
-email.addEventListener('input', throttle((e) => {
+// Rotate head on input
+email.addEventListener('input', throttle(() => {
   // Keep degree btw -15 and 15
   const length = Math.min(email.value.length - 16, 15) * -1;
   dog.style.setProperty('--rotate-face', `${length}deg`);
-}, 100))
+}, 100));
+
+// Cover face on focus
+password.addEventListener('focus', () => {
+  hands.forEach(hand => {
+    hand.classList.add('cover');
+  });
+  tongue.classList.add('hold');
+});
+
+// Hide face on blur
+password.addEventListener('blur', () => {
+  hands.forEach(hand => {
+    hand.classList.remove('cover');
+  });
+  tongue.classList.remove('hold');
+});
+
+toggle.addEventListener('click', () => {
+  const passType = password.getAttribute('type');
+  if (passType === 'password') {
+    password.setAttribute('type', 'text');
+  } else {
+    password.setAttribute('type', 'password');
+  }
+
+  if (hands[0].classList.contains('cover')) {
+    hands.forEach(hand => {
+      hand.classList.remove('cover');
+      hand.classList.add('peek');
+    });
+  } else if (hands[0].classList.contains('peek')) {
+    hands.forEach(hand => {
+      hand.classList.remove('peek');
+      hand.classList.add('cover');
+    });
+  } else {
+    hands.forEach(hand => {
+      hand.classList.toggle('peek');
+    });
+  }
+});
+
+// prevent page load
+const form = document.querySelector('form');
+const submit = document.querySelector('button[type=submit]');
+
+const prevent = e => {
+  e.preventDefault();
+}
+
+form.addEventListener('submit', prevent);
+submit.addEventListener('click', prevent);
