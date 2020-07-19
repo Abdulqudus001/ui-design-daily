49f8b4cde6608d666b7c81ccc99d39d687fbb4eb
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-07-19 13:42:33 +0100

    Added scroll page on animation end

--- welcome-loader/main.js
+++ welcome-loader/main.js
@@ -1,10 +1,29 @@
+// Scroll to top on reload
+document.body.style.overflowY = 'hidden';
+window.scrollTo({
+  top: 0,
+  behavior: 'smooth',
+});
+
 const scrollPage = () => {
-  console.log('object');
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: 'smooth',
+  });
 }
+
+const scrollTop = document.body.scrollTop
 
 // Detect animation end
 const image = document.querySelector('.welcome');
 
 image.addEventListener('animationend', () => {
-  setTimeout(scrollPage, 1000);
+  document.body.style.overflowY = 'auto';
+  if (scrollTop < 10) {
+    setTimeout(scrollPage, 2500);
+  }
+});
+
+document.querySelector('h1').addEventListener('animationend', () => {
+  document.querySelector('h2').style.opacity = '1';
 });
